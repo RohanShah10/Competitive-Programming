0bf8d4ecdd1bf312a1030b264216ef9bceebc8ab
RohanShah10 <83609184+RohanShah10@users.noreply.github.com>
2021-05-03 21:42:35 +0100

    Update 1-weirdalgorithm.py

--- CSES/1-weirdalgorithm.py
+++ CSES/1-weirdalgorithm.py
@@ -1,7 +1,12 @@
 n = int(input())
 while n!=1:
+    print(f"{n} ", end="")
     if n%2 == 0:
         n = n//2
     else:
         n = (n*3)+1
-    print(f"{n} ", end="")
+    
+print("1", end="")
+
+#Key thing that I learnt here was how to print without going onto a new line immediately after
+  
